12209c8bb31f58f044ceae1d05f53b5f026344de
Timo <38291523+lovetodream@users.noreply.github.com>
2022-02-23 20:08:14 +0100

    test(unit): add tests for data and url endpoints

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,19 @@
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {
+  DataSnapshotParamsDto,
+  SnapshotParamsDto,
+} from './dto/snapshot-params.dto';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule, ConfigModule.forRoot()],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -14,9 +21,37 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('snapshot', () => {
+    describe('url', () => {
+      it('should return a url with center at apple park', async () => {
+        const params = new SnapshotParamsDto();
+        params.center = 'apple park';
+
+        expect(appController.getSnapshotUrl(params)).toMatch(
+          /^https:\/\/snapshot\.apple-mapkit\.com\/api\/v1\/snapshot\?*center=apple%20park*/,
+        );
+      });
+    });
+
+    describe('data', () => {
+      it('should return valid data', async () => {
+        const params = new DataSnapshotParamsDto();
+        params.center = 'apple park';
+
+        expect(typeof (await appController.getSnapshotData(params))).toBe(
+          'string',
+        );
+      });
+
+      it('should return valid data with prefix', async () => {
+        const params = new DataSnapshotParamsDto();
+        params.center = 'apple park';
+        params.withPrefix = true;
+
+        expect(await appController.getSnapshotData(params)).toMatch(
+          /data:image\/png;base64,*/,
+        );
+      });
     });
   });
 });
