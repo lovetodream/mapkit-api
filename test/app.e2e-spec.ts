a955883a78c6bb8d85bb254ed6c6ee85a8a1a496
Timo <38291523+lovetodream@users.noreply.github.com>
2022-02-23 21:04:20 +0100

    test(e2e): add tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,55 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect((response) => {
+        expect(response.body.status).toBe('ok');
+      });
+  });
+
+  it('/snapshot/url (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/snapshot/url')
+      .send({ center: 'apple park' })
+      .expect(200)
+      .expect(
+        /^https:\/\/snapshot\.apple-mapkit\.com\/api\/v1\/snapshot\?*center=apple%20park*/,
+      );
+  });
+
+  describe('/snapshot/data (GET)', () => {
+    it('should return data without prefix', () => {
+      return request(app.getHttpServer())
+        .get('/snapshot/data')
+        .send({ center: 'apple park' })
+        .expect(200)
+        .expect('content-type', 'text/html; charset=utf-8');
+    });
+
+    it('should return data with prefix', () => {
+      return request(app.getHttpServer())
+        .get('/snapshot/data')
+        .send({ center: 'apple park', withPrefix: true })
+        .expect(200)
+        .expect('content-type', 'text/html; charset=utf-8')
+        .expect(/data:image\/png;base64,*/);
+    });
+
+    it('should fail due to bad request', () => {
+      return request(app.getHttpServer()).get('/snapshot/data').expect(400);
+    });
+  });
+
+  describe('/snapshot/png (GET)', () => {
+    it('should return a valid png', () => {
+      return request(app.getHttpServer())
+        .get('/snapshot/png')
+        .send({ center: 'apple park' })
+        .expect(200)
+        .expect('content-type', 'image/png');
+    });
+
+    it('should fail due to bad request', () => {
+      return request(app.getHttpServer()).get('/snapshot/png').expect(400);
+    });
   });
 });
